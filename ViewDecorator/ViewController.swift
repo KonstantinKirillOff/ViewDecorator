c029e2068b58b290ae058b4a7fd7e178f4b40147
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-08-22 20:03:49 +0300

    add UI elements and logic

--- ViewDecorator/ViewController.swift
+++ ViewDecorator/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mainView.layer.cornerRadius = 10
     }
 
-
+    @IBAction func redSliderChanged() {
+        redValueLabel.text = String(round(redSlider.value * 100) / 100)
+        collectColor(for: mainView)
+    }
+    
+    @IBAction func greenSliderChanged() {
+        greenValueLabel.text = String(round(greenSlider.value * 100) / 100)
+        collectColor(for: mainView)
+    }
+    
+    @IBAction func blueSliderChanged() {
+        blueValueLabel.text = String(round(blueSlider.value * 100) / 100)
+        collectColor(for: mainView)
+    }
+    
 }
 
+// MARK: - Privet functions
+extension ViewController {
+    private func collectColor(for view: UIView) {
+        let totalColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(1))
+        
+        view.backgroundColor = totalColor
+    }
+    
+}
