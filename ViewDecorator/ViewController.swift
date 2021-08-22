edaae4cb2027ea764374e29e3df466bec36216a7
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-08-22 20:55:38 +0300

    add constraints

--- ViewDecorator/ViewController.swift
+++ ViewDecorator/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         mainView.layer.cornerRadius = 10
+        collectColor(for: mainView)
     }
 
     @IBAction func redSliderChanged() {
